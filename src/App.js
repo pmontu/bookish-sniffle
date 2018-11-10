735bb81eb8b6b666e3151877ede156b848102d4b
pmontu <pmontu@gmail.com>
2018-11-10 16:30:07 +0530

    added upload button and canvas to 1st and 3rd tabs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 
+import 'typeface-roboto'
 import FileBase64 from 'react-file-base64';
-import {SketchField, Tools} from 'react-sketch';import PropTypes from 'prop-types';
+import { SketchField, Tools } from 'react-sketch';
+import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
+import Button from '@material-ui/core/Button';
 import AddToPhotosIcon from '@material-ui/icons/AddToPhotos';
 import PhotoLibraryIcon from '@material-ui/icons/PhotoLibrary';
 import BrushIcon from '@material-ui/icons/Brush';
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
 import Typography from '@material-ui/core/Typography';
 
 function TabContainer(props) {
@@ -16,6 +20,49 @@ function TabContainer(props) {
     <Typography component="div" style={{ padding: 8 * 3 }}>
       {props.children}
     </Typography>
+  );
+}
+
+class Upload extends React.Component {
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <div>
+        <input
+          accept="image/*"
+          className={classes.input}
+          style={{ display: 'none' }}
+          id="raised-button-file"
+          type="file"
+          ref={(ref) => this.myInput = ref}
+        />
+        <label htmlFor="raised-button-file">
+          <Button
+            variant="contained"
+            color="default"
+            className={classes.button}
+            onClick={(e) => this.myInput.click()}
+          >
+            Upload
+            <CloudUploadIcon className={classes.rightIcon} />
+          </Button>
+        </label>
+      </div>
+    );
+  }
+}
+
+function Canvas(props) {
+  return (
+    <SketchField
+      width='100%'
+      height='100%'
+      tool={Tools.Pencil}
+      lineColor='black'
+      lineWidth={3}
+      style={{"border-style": 'solid'}}
+    />
   );
 }
 
@@ -28,6 +75,12 @@ const styles = theme => ({
     flexGrow: 1,
     width: '100%',
     backgroundColor: theme.palette.background.paper,
+  },
+  button: {
+    margin: theme.spacing.unit,
+  },
+  rightIcon: {
+    marginLeft: theme.spacing.unit,
   },
 });
 
@@ -60,9 +113,9 @@ class App extends React.Component {
             <Tab label="Image Canvas" icon={<BrushIcon />} />
           </Tabs>
         </AppBar>
-        {value === 0 && <TabContainer>Item One</TabContainer>}
+        {value === 0 && <TabContainer><Upload classes={this.props.classes}/></TabContainer>}
         {value === 1 && <TabContainer>Item Two</TabContainer>}
-        {value === 2 && <TabContainer>Item Three</TabContainer>}
+        {value === 2 && <TabContainer><Canvas/></TabContainer>}
       </div>
     );
   }
